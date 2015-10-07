9c79a9c88d5f5a2410be02bfbc9ccec0441e2e6e
sliscers <info@localdesign.nl>
2015-10-07 11:59:22 +0200

    MergeCommit

--- WindesHeim Game/WindesHeim Game/Models.cs
+++ WindesHeim Game/WindesHeim Game/Models.cs
@@ -87,7 +87,8 @@ namespace WindesHeim_Game
 
             this.highscore.Click += new EventHandler(menuController.highscore_Click);
 
-
+            gameWindow.BackgroundImage = Resources.menuBackground;
+            gameWindow.BackgroundImage.
             gameWindow.Controls.Add(play);
             gameWindow.Controls.Add(editor);
             gameWindow.Controls.Add(highscore);
